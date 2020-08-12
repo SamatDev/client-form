5435deb230e4e596b7862def6b24c5328e11657c
Samat <5200_@mail.ru>
2020-08-12 14:35:03 +0300

    Initial dist subtree commit

---
+++ precache-manifest.2364b5cda3fbbf4dc916f49bec7e48a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3ae932683dc04bd837",
+    "url": "/client-form/css/app.ec4850bf.css"
+  },
+  {
+    "revision": "47f3b1533541d5ad230e08b8adaadc55",
+    "url": "/client-form/index.html"
+  },
+  {
+    "revision": "bc95c5a543a8565cf238",
+    "url": "/client-form/js/about.331b7329.js"
+  },
+  {
+    "revision": "0f3ae932683dc04bd837",
+    "url": "/client-form/js/app.5595a5cb.js"
+  },
+  {
+    "revision": "9ed6ffe8ee4c14ed7eda",
+    "url": "/client-form/js/chunk-vendors.d9eb7701.js"
+  },
+  {
+    "revision": "abc05d896f13aff9a9e1c1dbaafb371e",
+    "url": "/client-form/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/client-form/robots.txt"
+  }
+]);
